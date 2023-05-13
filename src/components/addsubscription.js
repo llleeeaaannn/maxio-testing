1dc987fcb8a227508d205f59c04b2669d3610759
llleeeaaannn <piercefrank10@gmail.com>
2023-05-13 14:09:00 +0100

    Create add subscription component

---
+++ src/components/addsubscription.js
@@ -0,0 +1,22 @@
+import styles from '../styles/addsubscription.module.css';
+
+const AddSubscription = () => {
+
+  const [subscriptionData, setSubscriptionData] = useState('')
+
+  return (
+    <div>
+
+      <div>
+
+      </div>
+
+      <div>
+        <button type="button">Create Subscription with random data</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default AddSubscription;
